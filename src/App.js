f65aa776a8ec72199c9e5a8cdd7b2b960fbe321e
dougiehawes <dougiehawes@hotmail.com>
2025-02-05 16:21:56 +0000

    added songs

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ import {
 import unchainImage from "./media/unchainMyHeart.webp";
 import valentineImage from "./media/myFunnyValentine.webp";
 import handleImage from "./media/hardToHandle.webp";
+import minnieImage from "./media/minnieTheMoocher.webp";
 import mustangImage from "./media/mustangSally.jpg";
 import kissImage from "./media/kiss.jpg";
 import ladyImage from "./media/ladyIsATramp.jpg";
@@ -25,6 +26,10 @@ import ladyImage from "./media/ladyIsATramp.jpg";
 import unchainMyHeart from "./media/unchainMyHeart.mp3";
 import myFunnyValentine from "./media/myFunnyValentine.mp3";
 import hardToHandle from "./media/hardToHandle.mp3";
+import minnieTheMoocher from "./media/minnieTheMoocher.mp3";
+import mustangSally from "./media/mustangSally.mp3";
+import kiss from "./media/kiss.mp3";
+import ladyIsATramp from "./media/ladyIsATramp.mp3";
 
 import "./style.scss";
 
@@ -50,6 +55,8 @@ const App = () => {
   }, [playing]);
 
   useEffect(() => {
+    setSongTime(0);
+
     const updateDuration = () => {
       if (audioRef.current && !isNaN(audioRef.current.duration)) {
         const totalSeconds = Math.floor(audioRef.current.duration);
@@ -118,19 +125,28 @@ const App = () => {
       image: handleImage,
       mp3: hardToHandle,
     },
-    // {
-    //   i: 3,
-    //   title: "Mustang Sally",
-    //   artist: "Wilson Pickett",
-    //   image: mustangImage,
-    // },
-    // { i: 4, title: "Kiss", artist: "Prince", image: kissImage },
-    // {
-    //   i: 5,
-    //   title: "The Lady is a Tramp",
-    //   artist: "Frank Sinatra",
-    //   image: ladyImage,
-    // },
+    {
+      i: 3,
+      title: "Minnie the Moocher",
+      artist: "Cab Calloway",
+      image: minnieImage,
+      mp3: minnieTheMoocher,
+    },
+    {
+      i: 4,
+      title: "Mustang Sally",
+      artist: "Wilson Pickett",
+      image: mustangImage,
+      mp3: mustangSally,
+    },
+    { i: 5, title: "Kiss", artist: "Prince", image: kissImage, mp3: kiss },
+    {
+      i: 6,
+      title: "The Lady is a Tramp",
+      artist: "Frank Sinatra",
+      image: ladyImage,
+      mp3: ladyIsATramp,
+    },
   ];
 
   const getPrevSong = () => {
